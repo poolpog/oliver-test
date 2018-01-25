b3399bec0fc32538e598b260399d5f22465907c5
Pool Pog <poolpog@gmail.com>
2018-01-24 20:46:44 -0500

    first run through of basic programming skills

---
+++ test_game.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+import time
+
+print "hi"
+name = raw_input("wat is ur name?")
+
+time.sleep(.2)
+
+print "Hi {0}! u r noice".format(name)
+
+time.sleep(.2)
+
+for i in range(5):
+    print "."
+    time.sleep(1)
+
+print "night..."
+
+time.sleep(2)
+print "You hear a froitening SCREAAAaaammmmmmmm.....!!!!"
+time.sleep(2)
+print "U hear the rustle of a paper rustling. Quickly, u swirtch on dem lites."
+print "There is a PAPER on da ground in froint of YOU."
+time.sleep(2)
+print "THIS"
+time.sleep(1)
+print "IS"
+time.sleep(1)
+print "WHAT"
+time.sleep(1)
+print "IT"
+time.sleep(1)
+print "SAYS..."
+
+time.sleep(1)
+
+print "Your person is safe with me"
+print "If you want dem bek you gotta find me"
+print "and beat me up. I guess."
+print "i dant no"
+print
+print
+time.sleep(2)
+print "TO BE CONTINUED.... Maybe like this weekend in the actual game?"
+
